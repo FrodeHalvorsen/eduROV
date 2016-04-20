d43b71885922944827f2ef24f4689f6b9e335f6b
Bernt-Johan Bergshaven <bbergshaven@gmail.com>
2016-04-20 12:39:05 +0200

    Added golang code

---
+++ EduRov-Arduino/EduRov-Arduino.ino
@@ -0,0 +1,93 @@
+// string to keep data received over serial
+String readString;
+
+// motor control outputs
+const int motorRearLeft = 2;
+const int motorRearRight = 3;
+const int motorFrontLeft = 4;
+const int motorFrontRight = 5;
+
+// led outputs
+const int ledPin = 13;
+
+void setup() {
+  Serial.begin(9600);
+  // outputs
+  pinMode(motorRearLeft, OUTPUT);
+  pinMode(motorRearRight, OUTPUT);
+  pinMode(motorFrontLeft, OUTPUT);
+  pinMode(motorFrontRight, OUTPUT);
+  pinMode(ledPin, OUTPUT); 
+}
+
+unsigned int i=0;
+
+void loop() {
+
+  while (Serial.available()) {
+    delay(3);  
+    char c = Serial.read();
+    readString += c; 
+  }
+  readString.trim();
+  if (readString.length() >0) {
+      if(readString == "left"){
+        motorLeft();
+      }
+      if(readString == "right"){
+        motorRight();
+      }
+      if(readString == "bck"){
+        motorBackward();
+      }
+      if(readString == "fwd"){
+        motorForward();
+      }
+      if(readString == "stop"){
+        motorStop();
+      }
+        readString = "";
+  }
+
+  if(i>=64000){
+    Serial.println("TestData");
+    i=0;
+  }
+  i++;
+
+}
+
+void motorLeft(){
+  digitalWrite(motorRearLeft, LOW);
+  digitalWrite(motorRearRight,HIGH);
+  digitalWrite(motorFrontLeft,HIGH);
+  digitalWrite(motorFrontRight,LOW);  
+}
+
+void motorRight(){
+  digitalWrite(motorRearLeft, HIGH);
+  digitalWrite(motorRearRight,LOW);
+  digitalWrite(motorFrontLeft,LOW);
+  digitalWrite(motorFrontRight,HIGH);  
+}
+
+void motorForward(){
+  digitalWrite(motorRearLeft, HIGH);
+  digitalWrite(motorRearRight,HIGH);
+  digitalWrite(motorFrontLeft, LOW);
+  digitalWrite(motorFrontRight, LOW);
+}
+
+void motorBackward(){
+  digitalWrite(motorRearLeft, LOW);
+  digitalWrite(motorRearRight,LOW);
+  digitalWrite(motorFrontLeft,HIGH);
+  digitalWrite(motorFrontRight,HIGH);
+}
+
+void motorStop(){
+  digitalWrite(motorRearLeft, LOW);
+  digitalWrite(motorRearRight,LOW);
+  digitalWrite(motorFrontLeft, LOW);
+  digitalWrite(motorFrontRight, LOW);
+}
---
+++ rovcontrol/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/desertbit/glue"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+
+	"github.com/jacobsa/go-serial/serial"
+)
+
+//Cmd ...
+type Cmd struct {
+	Action string `json:"action"`
+}
+
+var port io.ReadWriteCloser
+var readChan chan string
+
+func init() {
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        "/dev/cu.usbmodem141321",
+		BaudRate:        9600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 10,
+	}
+
+	var err error
+	// Open the port.
+	port, err = serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+}
+
+func main() {
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	readChan = make(chan string)
+
+	go serialReaderHandler(readChan)
+
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./html", true)))
+	r.Use(static.Serve("/dist/", static.LocalFile("./dist", true)))
+
+	r.GET("/api", testHandler)
+
+	// Create a new glue server.
+	server := glue.NewServer(glue.Options{
+		HTTPSocketType: glue.HTTPSocketTypeNone,
+	})
+
+	// Release the glue server on defer.
+	// This will block new incoming connections
+	// and close all current active sockets.
+	defer server.Release()
+
+	r.Use(gin.WrapH(server))
+
+	// Set the glue event function to handle new incoming socket connections.
+	server.OnNewSocket(onNewSocket)
+
+	log.Println("Listening on :8080")
+
+	r.Run(":8080")
+}
+
+func testHandler(c *gin.Context) {
+
+	c.String(300, "Works")
+
+}
+
+func onNewSocket(s *glue.Socket) {
+
+	// We won't read any data from the socket itself.
+	// Discard received data!
+	s.DiscardRead()
+
+	// Set a function which is triggered as soon as the socket is closed.
+	s.OnClose(func() {
+		log.Printf("socket closed with remote address: %s", s.RemoteAddr())
+	})
+
+	// Set a function which is triggered during each received message.
+	s.OnRead(func(data string) {
+		// Echo the received data back to the client.
+		s.Write(data)
+	})
+
+	// Create a channel.
+	c := s.Channel("control")
+
+	// Set the channel on read event function.
+	c.OnRead(func(data string) {
+		var cmd Cmd
+		err := json.Unmarshal([]byte(data), &cmd)
+		if err != nil {
+			log.Println("Error decoding ctrl msg", err)
+			return
+		}
+		fmt.Println("Got action", cmd.Action)
+		port.Write([]byte(cmd.Action + "\r"))
+
+		// Echo the received data back to the client.
+		c.Write("ack: " + cmd.Action)
+	})
+
+	// Send a welcome string to the client.
+	s.Write("Hello Client")
+}
+
+func serialReaderHandler(r chan string) {
+	reader := bufio.NewReader(port)
+	for {
+		reply, err := reader.ReadBytes('\n')
+
+		if err != nil {
+			log.Printf("Could not read from serial: %s\n", err)
+		}
+
+		s := string(reply)
+		fmt.Printf("Got from serial: %s", s)
+		//r <- s
+
+	}
+
+}
