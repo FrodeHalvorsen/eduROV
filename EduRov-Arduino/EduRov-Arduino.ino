9c132057c7cb95357b365d6676798843becee477
Frode Halvorsen <halvorsn@gmail.com>
2016-04-21 14:45:55 +0200

    Update EduRov-Arduino.ino
    Added two motors

--- EduRov-Arduino/EduRov-Arduino.ino
+++ EduRov-Arduino/EduRov-Arduino.ino
@@ -2,10 +2,12 @@
 String readString;
 
 // motor control outputs
-const int motorRearLeft = 2;
-const int motorRearRight = 3;
-const int motorFrontLeft = 4;
-const int motorFrontRight = 5;
+const int motorRear1 = 2; // 12 oclock
+const int motorRear2 = 3; // 4 oclock
+const int motorRear3 = 4; // 8 oclock
+const int motorFront1 = 5; // // 2oclock
+const int motorFront2 = 6; // 6 oclock
+const int motorFront3 = 7; // 10 oclock
 
 // led outputs
 const int ledPin = 13;
@@ -13,10 +15,12 @@ const int ledPin = 13;
 void setup() {
   Serial.begin(9600);
   // outputs
-  pinMode(motorRearLeft, OUTPUT);
-  pinMode(motorRearRight, OUTPUT);
-  pinMode(motorFrontLeft, OUTPUT);
-  pinMode(motorFrontRight, OUTPUT);
+  pinMode(motorRear1, OUTPUT);
+  pinMode(motorRear2, OUTPUT);
+  pinMode(motorRear3, OUTPUT);
+  pinMode(motorFront1, OUTPUT);
+  pinMode(motorFront2, OUTPUT);
+  pinMode(motorFront3, OUTPUT);
   pinMode(ledPin, OUTPUT); 
 }
 
@@ -58,36 +62,46 @@ void loop() {
 }
 
 void motorLeft(){
-  digitalWrite(motorRearLeft, LOW);
-  digitalWrite(motorRearRight,HIGH);
-  digitalWrite(motorFrontLeft,HIGH);
-  digitalWrite(motorFrontRight,LOW);  
+  digitalWrite(motorRear1, LOW);
+  digitalWrite(motorRear2,HIGH);
+  digitalWrite(motorRear3,LOW);
+  digitalWrite(motorFront1,HIGH);
+  digitalWrite(motorFront2,LOW); 
+  digitalWrite(motorFront3,LOW);
 }
 
 void motorRight(){
-  digitalWrite(motorRearLeft, HIGH);
-  digitalWrite(motorRearRight,LOW);
-  digitalWrite(motorFrontLeft,LOW);
-  digitalWrite(motorFrontRight,HIGH);  
+  digitalWrite(motorRear1, LOW);
+  digitalWrite(motorRear2,LOW);
+  digitalWrite(motorRear3,HIGH);
+  digitalWrite(motorFront1,LOW);
+  digitalWrite(motorFront2,LOW); 
+  digitalWrite(motorFront3,HIGH);  
 }
 
 void motorForward(){
-  digitalWrite(motorRearLeft, HIGH);
-  digitalWrite(motorRearRight,HIGH);
-  digitalWrite(motorFrontLeft, LOW);
-  digitalWrite(motorFrontRight, LOW);
+  digitalWrite(motorRear1,HIGH);
+  digitalWrite(motorRear2,HIGH);
+  digitalWrite(motorRear3,HIGH);
+  digitalWrite(motorFront1,LOW);
+  digitalWrite(motorFront2,LOW); 
+  digitalWrite(motorFront3,LOW);
 }
 
 void motorBackward(){
-  digitalWrite(motorRearLeft, LOW);
-  digitalWrite(motorRearRight,LOW);
-  digitalWrite(motorFrontLeft,HIGH);
-  digitalWrite(motorFrontRight,HIGH);
+  digitalWrite(motorRear1, LOW);
+  digitalWrite(motorRear2,LOW);
+  digitalWrite(motorRear3,LOW);
+  digitalWrite(motorFront1,HIGH);
+  digitalWrite(motorFront2,HIGH); 
+  digitalWrite(motorFront3,HIGH);
 }
 
 void motorStop(){
-  digitalWrite(motorRearLeft, LOW);
-  digitalWrite(motorRearRight,LOW);
-  digitalWrite(motorFrontLeft, LOW);
-  digitalWrite(motorFrontRight, LOW);
+  digitalWrite(motorRear1, LOW);
+  digitalWrite(motorRear2,LOW);
+  digitalWrite(motorRear3,LOW);
+  digitalWrite(motorFront1,LOW);
+  digitalWrite(motorFront2,LOW); 
+  digitalWrite(motorFront3,LOW);
 }
